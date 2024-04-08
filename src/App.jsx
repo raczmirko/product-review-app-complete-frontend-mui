f3dd6ec25eb8226c62f4cea4e3974a53f0a3d60b
raczmirko <m1rk0d3v@gmail.com>
2024-04-08 14:26:19 +0200

    feat: added isLoggedIn hook to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from '@mui/material';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>
+            <Sidebar isLoggedIn={isLoggedIn}/>
             <Typography variant='h1'>Hello World!</Typography>
         </div>
     )
