2b28c7b9647ed2e8a52e17ffc200a78459be3d98
raczmirko <m1rk0d3v@gmail.com>
2024-04-09 09:43:09 +0200

    feat: added particles dependency and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Typography } from '@mui/material';
 import Sidebar from './components/Sidebar';
+import ParticleBackground from './components/ParticleBackground';
 
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>
             <Sidebar isLoggedIn={isLoggedIn}/>
+            <ParticleBackground />
             <Typography variant='h1'>Hello World!</Typography>
         </div>
     )
