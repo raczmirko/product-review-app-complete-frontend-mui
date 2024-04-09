fa8d2f77bd4974e6290fdb56076cb4aa9212884e
raczmirko <m1rk0d3v@gmail.com>
2024-04-09 22:56:23 +0200

    refactor: removed unused import

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -13,7 +13,6 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AlertSnackBar from '../components/AlertSnackBar';
 import CountrySelector from '../components/CountrySelector';
-import FormControl from '@mui/material/FormControl';
 
 function Copyright(props) {
   return (
