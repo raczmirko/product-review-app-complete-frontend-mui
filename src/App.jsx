e5f381e2a9b937beab9d870a9d2125f436436264
raczmirko <m1rk0d3v@gmail.com>
2024-04-09 10:20:54 +0200

    feat: enabled dark mode by default

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import React, { useState } from 'react';
 import { Typography } from '@mui/material';
 import Sidebar from './components/Sidebar';
 import ParticleBackground from './components/ParticleBackground';
+import { ThemeContext, ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
-        <div>
-            <Sidebar isLoggedIn={isLoggedIn}/>
-            <ParticleBackground />
-            <Typography variant='h1'>Hello World!</Typography>
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <div>
+                <Sidebar isLoggedIn={isLoggedIn}/>
+                <ParticleBackground />
+                <Typography variant='h1'>Hello World!</Typography>
+            </div>
+        </ThemeProvider>
     )
 }
 
