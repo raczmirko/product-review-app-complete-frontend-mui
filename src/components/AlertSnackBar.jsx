94398d68639f60f0f003f3b7485e17470da8f224
raczmirko <m1rk0d3v@gmail.com>
2024-04-09 14:20:57 +0200

    feat: added signIn page and snackBar alerts

---
+++ src/components/AlertSnackBar.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+export default function AlertSnackBar({ alertType, alertText, isOpen, setIsOpen }) {
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={alertType}
+          variant="filled"
+          sx={{ width: '100%' }}
+        >
+          {alertText}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
