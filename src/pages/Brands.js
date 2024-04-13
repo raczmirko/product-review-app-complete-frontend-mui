e40902719c54f36b42052719949c63b1f38c4253
raczmirko <m1rk0d3v@gmail.com>
2024-04-13 09:10:53 +0200

    feat: added brands page

---
+++ src/pages/Brands.js
@@ -0,0 +1,21 @@
+import PageHeader from "../components/PageHeader";
+import Container from '@mui/material/Container';
+import ParticleBackground from '../components/ParticleBackground';
+import BrandTable from '../components/tables/BrandTable';
+
+const Brands = () => {
+
+    return (
+        <Container container component="main" sx={{ marginTop:'20px' , padding:'10px', justifyContent: 'center'}}>
+            <Container sx={{position: 'absolute', zIndex:-1}}>
+                <ParticleBackground />
+            </Container>
+            <Container >
+                <PageHeader text="Brands" color="#81BE83" textColor="white"/>
+                <BrandTable/>
+             </Container>
+        </Container>
+      );
+};
+
+export default Brands;
