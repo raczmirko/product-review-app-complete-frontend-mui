0fdc66c88aff86d9897e5f8dbf31a13d4f155a55
raczmirko <m1rk0d3v@gmail.com>
2024-04-16 21:44:44 +0200

    feat: made the confirmation dialog draggable

--- src/components/ConfirmationDialog.jsx
+++ src/components/ConfirmationDialog.jsx
@@ -1,38 +1,48 @@
+import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Draggable from 'react-draggable';
+
+function PaperComponent(props) {
+    return (
+        <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
+            <Paper {...props} />
+        </Draggable>
+    );
+}
 
 export default function ConfirmationDialog({ dialogTitle, dialogDescription, isOpen, setIsOpen, functionToRunOnConfirm }) {
+    
+      const handleConfirm = () => {
+        setIsOpen(false);
+        functionToRunOnConfirm();
+    };    
 
     return (
-        <Dialog
-            open={isOpen}
-            onClose={() => setIsOpen(false)}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-        >
-            <DialogTitle id="alert-dialog-title">{dialogTitle}</DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    {dialogDescription}
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
+    <Dialog
+        open={isOpen}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+    >
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+            {dialogTitle}
+        </DialogTitle>
+        <DialogContent>
+            <DialogContentText>
+                {dialogDescription}
+            </DialogContentText>
+        </DialogContent>
+        <DialogActions>
             <Button onClick={() => setIsOpen(false)}>Cancel</Button>
-            <Button
-                onClick={() => {
-                    setIsOpen(false);
-                    functionToRunOnConfirm();
-                }}
-                color="warning"
-                autoFocus
-            >
+            <Button onClick={handleConfirm} color="warning" autoFocus>
                 Confirm
             </Button>
-            </DialogActions>
-        </Dialog>
+        </DialogActions>
+    </Dialog>
     );
 }
