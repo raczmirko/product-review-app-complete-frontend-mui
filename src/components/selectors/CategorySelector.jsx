658d8b4f95f46d56da35e50c81a82e3e09847943
raczmirko <m1rk0d3v@gmail.com>
2024-04-27 16:40:20 +0200

    feat: added CategorySelector

---
+++ src/components/selectors/CategorySelector.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import CategoryService from '../../services/CategoryService';
+
+const CategorySelector = ({ selectedCategory, setSelectedCategory }) => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        // Fetch categories when the component mounts
+        CategoryService.fetchCategories()
+            .then(data => setCategories(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    const handleChange = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="category-select-label">Category</InputLabel>
+            <Select
+            labelId="category-select-label"
+            variant='filled'
+            id="category-select"
+            value={selectedCategory}
+            label="Category"
+            onChange={(e) => handleChange(e)}
+            >
+                {categories.map(category => (
+                    <MenuItem key={category.id} value={category}>{category.name}</MenuItem>
+                ))}
+            </Select>
+      </FormControl>
+    );
+};
+
+export default CategorySelector;
