13649dc3b36d7c3c4c28f32fdc89a8502918e21d
raczmirko <m1rk0d3v@gmail.com>
2024-04-27 12:14:34 +0200

    refactor: created selectors folder and moved CountrySelector

--- src/components/selectors/CountrySelector.jsx
+++ src/components/selectors/CountrySelector.jsx
@@ -4,7 +4,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import CountryService from '../services/CountryService';
+import CountryService from '../../services/CountryService';
 
 const CountrySelector = ({ selectedCountry, setSelectedCountry }) => {
     const [countries, setCountries] = useState([]);
