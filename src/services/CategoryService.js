9da214872717e7b3e03a2e751dd08e5cb23bdc89
raczmirko <m1rk0d3v@gmail.com>
2024-05-03 22:28:59 +0200

    feat: added featchCategoryTree method to CategoryService

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -17,6 +17,26 @@ class CategoryService {
             return [];
         }
     }
+
+    static async fetchCategoryTree(id) {
+        const token = localStorage.getItem('token');
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        };
+        try {
+            const response = await fetch(`http://localhost:8080/category-hierarchy/${id}`, {headers});
+            if (!response.ok) {
+                throw new Error('Failed to fetch category tree');
+            }
+            const data = await response.json();
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.error('Error fetching category tree:', error);
+            return [];
+        }
+    }
 }
 
 export default CategoryService;
