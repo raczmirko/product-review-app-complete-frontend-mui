1734d32cf7ce8002c7a07777401ac9dfa5ffc2b6
raczmirko <m1rk0d3v@gmail.com>
2024-05-04 10:27:50 +0200

    feat: CategoryTree is expanded by default and current category is selected by default

--- src/components/CategoryTreeRenderer.js
+++ src/components/CategoryTreeRenderer.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
 
 const renderCategoryTree = (categoryTree) => {
     if (!categoryTree || !categoryTree.currentCategory) {
@@ -10,60 +11,75 @@ const renderCategoryTree = (categoryTree) => {
 
     const { currentCategory, currentParentCategory, currentSubcategories, subSubcategories, parentParentCategory } = categoryTree;
 
+    {/* Expand the tree until current category and select current category*/}
+    const selectedItemIDs = [];
+    const expandedItemIDs = [];
+    if (parentParentCategory) {
+        expandedItemIDs.push(parentParentCategory.id);
+    }
+    if (currentParentCategory) {
+        expandedItemIDs.push(currentParentCategory.id);
+    }
+    expandedItemIDs.push(currentCategory.id);
+    selectedItemIDs.push(currentCategory.id);
+
     return (
         <>
-            {/* Render from parentParentCategory if exists */}
-            {parentParentCategory && (
-                <TreeItem itemId={parentParentCategory.id} label={parentParentCategory.name}>
-                    {/* Render currentParentCategory */}
-                    {currentParentCategory && (
-                        <TreeItem itemId={currentParentCategory.id} label={currentParentCategory.name}>
-                            {/* Render currentCategory */}
-                            <TreeItem itemId={currentCategory.id} label={currentCategory.name}>
-                                {/* Render currentSubcategories */}
-                                {currentSubcategories.map(subcategory => (
-                                    <TreeItem itemId={subcategory.id} label={subcategory.name}>
-                                        {/* Render subSubcategories */}
-                                        {subSubcategories[subcategory.id] && subSubcategories[subcategory.id].map(subSubcategory => (
-                                            <TreeItem itemId={subSubcategory.id} label={subSubcategory.name} />
-                                        ))}
-                                    </TreeItem>
+            <SimpleTreeView defaultExpandedItems={expandedItemIDs} defaultSelectedItems={selectedItemIDs}>
+                {/* Render from parentParentCategory if exists */}
+                {parentParentCategory && (
+                    <TreeItem itemId={parentParentCategory.id} label={parentParentCategory.name}>
+                        {/* Render currentParentCategory */}
+                        {currentParentCategory && (
+                            <TreeItem itemId={currentParentCategory.id} label={currentParentCategory.name}>
+                                {/* Render currentCategory */}
+                                <TreeItem itemId={currentCategory.id} label={currentCategory.name}>
+                                    {/* Render currentSubcategories */}
+                                    {currentSubcategories.map(subcategory => (
+                                        <TreeItem itemId={subcategory.id} label={subcategory.name}>
+                                            {/* Render subSubcategories */}
+                                            {subSubcategories[subcategory.id] && subSubcategories[subcategory.id].map(subSubcategory => (
+                                                <TreeItem itemId={subSubcategory.id} label={subSubcategory.name} />
+                                            ))}
+                                        </TreeItem>
+                                    ))}
+                                </TreeItem>
+                            </TreeItem>
+                        )}
+                    </TreeItem>
+                )}
+                {/* Render from currentParentCategory if exists */}
+                {!parentParentCategory && currentParentCategory && (
+                    <TreeItem itemId={currentParentCategory.id} label={currentParentCategory.name}>
+                        {/* Render currentCategory */}
+                        <TreeItem itemId={currentCategory.id} label={currentCategory.name}>
+                            {/* Render currentSubcategories */}
+                            {currentSubcategories.map(subcategory => (
+                                <TreeItem itemId={subcategory.id} label={subcategory.name}>
+                                    {/* Render subSubcategories */}
+                                    {subSubcategories[subcategory.id] && subSubcategories[subcategory.id].map(subSubcategory => (
+                                        <TreeItem itemId={subSubcategory.id} label={subSubcategory.name} />
+                                    ))}
+                                </TreeItem>
+                            ))}
+                        </TreeItem>
+                    </TreeItem>
+                    
+                )}
+                {/* Render from currentCategory */}
+                {!parentParentCategory && !currentParentCategory && (
+                    <TreeItem itemId={currentCategory.id} label={currentCategory.name}>
+                        {currentSubcategories.map(subcategory => (
+                            <TreeItem itemId={subcategory.id} label={subcategory.name}>
+                                {/* Render subSubcategories */}
+                                {subSubcategories[subcategory.id] && subSubcategories[subcategory.id].map(subSubcategory => (
+                                    <TreeItem itemId={subSubcategory.id} label={subSubcategory.name} />
                                 ))}
                             </TreeItem>
-                        </TreeItem>
-                    )}
-                </TreeItem>
-            )}
-            {/* Render from currentParentCategory if exists */}
-            {!parentParentCategory && currentParentCategory && (
-                <TreeItem itemId={currentParentCategory.id} label={currentParentCategory.name}>
-                {/* Render currentCategory */}
-                <TreeItem itemId={currentCategory.id} label={currentCategory.name}>
-                    {/* Render currentSubcategories */}
-                    {currentSubcategories.map(subcategory => (
-                        <TreeItem itemId={subcategory.id} label={subcategory.name}>
-                            {/* Render subSubcategories */}
-                            {subSubcategories[subcategory.id] && subSubcategories[subcategory.id].map(subSubcategory => (
-                                <TreeItem itemId={subSubcategory.id} label={subSubcategory.name} />
-                            ))}
-                        </TreeItem>
-                    ))}
-                </TreeItem>
-            </TreeItem>
-            )}
-            {/* Render from currentCategory */}
-            {!parentParentCategory && !currentParentCategory && (
-                <TreeItem itemId={currentCategory.id} label={currentCategory.name}>
-                    {currentSubcategories.map(subcategory => (
-                        <TreeItem itemId={subcategory.id} label={subcategory.name}>
-                            {/* Render subSubcategories */}
-                            {subSubcategories[subcategory.id] && subSubcategories[subcategory.id].map(subSubcategory => (
-                                <TreeItem itemId={subSubcategory.id} label={subSubcategory.name} />
-                            ))}
-                        </TreeItem>
-                    ))}
-                </TreeItem>
-            )}
+                        ))}
+                    </TreeItem>
+                )}
+            </SimpleTreeView>
         </>
     );
 };
