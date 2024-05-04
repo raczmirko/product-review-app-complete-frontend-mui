79f9f3e5b13b7a991f6a7c741ec44737f4642b45
raczmirko <m1rk0d3v@gmail.com>
2024-05-04 19:23:44 +0200

    feat: added NotificationService to handle custom notifications

---
+++ src/services/NotificationService.js
@@ -0,0 +1,19 @@
+const getCustomNotification = (code, message) => {
+    let text = code + ": An error occurred, please try again later!";
+    if(code === 400) {
+        text = code + ": A country with this name or country code might already exist.";
+    }
+    if(code === 401) {
+        text = code + ": Authentication failed. Log in again!";
+    }
+    if(code === 403) {
+        text = code + ": You cannot access this page. Your session might have expired or you might need admin privileges to view.";
+    }
+    if(code === 404) {
+        text = code + ": NOT FOUND.";
+    }
+    if(code === 409) {
+        text = code + ": " + message;
+    }
+    return text;
+}
