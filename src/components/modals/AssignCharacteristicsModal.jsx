b3764dfc696c4f8526af5987d2239407507cf64b
raczmirko <m1rk0d3v@gmail.com>
2024-05-12 17:57:21 +0200

    feat: added assignCharacteristicsModal and functionality

--- src/components/modals/AssignCharacteristicsModal.jsx
+++ src/components/modals/AssignCharacteristicsModal.jsx
@@ -11,19 +11,50 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import Paper from '@mui/material/Paper';
-import { textAlign } from '@mui/system';
+import CharacteristicService from '../../services/CharacteristicService';
+import NotificationService from '../../services/NotificationService';
 
 const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOpen }) => {
     const [category, setCategory] = useState([]);
     const [checked, setChecked] = React.useState([]);
-    const [left, setLeft] = React.useState([0, 1, 2, 3]);
-    const [right, setRight] = React.useState([4, 5, 6, 7]);
+    const [left, setLeft] = React.useState([]);
+    const [right, setRight] = React.useState([]);
     const leftChecked = intersection(checked, left);
     const rightChecked = intersection(checked, right);
     
+    const modifyEntity = async (newCategory) => {
+        const token = localStorage.getItem('token');
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        };
+
+        try {
+            const response = await fetch(`http://localhost:8080/category/${newCategory.id}/modify`, {
+                method: 'PUT',
+                headers,
+                body: JSON.stringify(newCategory)
+            });
+
+            if (!response.ok) {
+                const errorMessage = NotificationService.getCustomNotification(response.status, await response.text());
+                throw new Error('Failed to update category.');
+            }
+        } catch (error) {
+            console.error('Error modifying category:', error);
+        }
+    };
+
     const handleClose = () => {
         setIsOpen(false);
         closeFunction();
+    }
+
+    const handleSave = () => {
+        const updatedCategory = category;
+        updatedCategory.characteristics = right;
+        modifyEntity(updatedCategory);
+        handleClose();
     }
 
     const handleToggle = (value) => () => {
@@ -65,11 +96,11 @@ const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOp
         <Paper sx={{ width: 200, height: 230, overflow: 'auto' }}>
           <List dense component="div" role="list">
             {items.map((value) => {
-              const labelId = `transfer-list-item-${value}-label`;
+              const labelId = `transfer-list-item-${value.id}-label`;
     
               return (
                 <ListItemButton
-                  key={value}
+                  key={value.id}
                   role="listitem"
                   onClick={handleToggle(value)}
                 >
@@ -83,25 +114,13 @@ const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOp
                       }}
                     />
                   </ListItemIcon>
-                  <ListItemText id={labelId} primary={`List item ${value + 1}`} />
+                  <ListItemText id={labelId} primary={value.name} secondary={`ID: ${value.id}`} />
                 </ListItemButton>
               );
             })}
           </List>
         </Paper>
     );
-
-    useEffect(() => {
-        setCategory(undefined);
-        // Fetch category tree when the component mounts
-        if (categoryId != undefined) {
-            CategoryService.getCategory(categoryId)
-            .then(data => {
-                setCategory(data);
-            })
-            .catch(error => console.error('Error:', error));
-        }
-    }, []);
 
     function not(a, b) {
         return a.filter((value) => b.indexOf(value) === -1);
@@ -111,6 +130,25 @@ const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOp
         return a.filter((value) => b.indexOf(value) !== -1);
     }
 
+    useEffect(() => {
+        setCategory(undefined);
+        setLeft([]);
+        setRight([]);
+        // Fetch category tree when the component mounts
+        if (categoryId != undefined) {
+            CategoryService.getCategory(categoryId)
+            .then(data => {
+                setCategory(data);
+                setRight(data.characteristics);
+            })
+            .catch(error => console.error('Error:', error));
+            CharacteristicService.fetchAvailableCharacteristics(categoryId)
+            .then(data => {
+                setLeft(data);
+            })
+        }
+    }, []);
+
     return (
         <>
         {/* Only render the modal when the category is fetched */}
@@ -118,8 +156,8 @@ const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOp
             <Modal
                 open={isOpen}
                 onClose={handleClose}
-                aria-labelledby="create-brand-modal"
-                aria-describedby="modal-to-create-brand"
+                aria-labelledby="assign-characteristics-modal"
+                aria-describedby="modal-to-assign-characteristics"
             >
                 <Box
                     sx={{
@@ -136,6 +174,8 @@ const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOp
                     }}
                 >
                     <Typography variant="h5" component="div" gutterBottom>Assign characteristics to {category.name}</Typography>
+                    <hr/>
+                    <Typography variant="p" component="div" gutterBottom>Characteristics are inherited from parent categories.</Typography>
                     <hr/>
                     <Grid container spacing={2} justifyContent="center" alignItems="center">
                         <Grid item>{customList(left)}</Grid>
@@ -185,7 +225,8 @@ const AssignCharacteristicsModal = ({ categoryId, closeFunction, isOpen, setIsOp
                         </Grid>
                         <Grid item>{customList(right)}</Grid>
                     </Grid>
-                    <Box sx={{ textAlign: 'right' }}>
+                    <Box sx={{ textAlign: 'right'}}>
+                        <Button variant="contained" color="primary" sx={{ mr: 1 }} onClick={handleSave}>Save</Button>
                         <Button variant="contained" color="secondary" onClick={handleClose}>Close</Button>
                     </Box>
                 </Box>
--- src/services/CharacteristicService.js
+++ src/services/CharacteristicService.js
@@ -16,6 +16,25 @@ class CharacteristicService {
             console.error('Error fetching assigned category tree:', error);
             return [];
         }
+    };
+
+    static async fetchAvailableCharacteristics(categoryId) {
+        const token = localStorage.getItem('token');
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        };
+        try {
+            const response = await fetch(`http://localhost:8080/characteristic/${categoryId}/available-characteristics`, {headers});
+            if (!response.ok) {
+                throw new Error('Failed to fetch assigned available characteristics');
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error fetching assigned available characteristics:', error);
+            return [];
+        }
     }
 }
 
