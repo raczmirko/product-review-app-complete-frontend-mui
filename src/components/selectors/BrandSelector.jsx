8f26851575e8ed539198c69f1f1e1c4ffef2cdb8
raczmirko <m1rk0d3v@gmail.com>
2024-05-18 08:47:51 +0200

    feat: created BrandSelector

---
+++ src/components/selectors/BrandSelector.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import BrandService from '../../services/BrandService';
+
+const BrandSelector = ({ selectedBrand, setSelectedBrand }) => {
+    const [brands, setBrands] = useState([]);
+
+    useEffect(() => {
+        // Fetch brands when the component mounts
+        BrandService.fetchBrands()
+            .then(data => setBrands(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    const handleChange = (event) => {
+        setSelectedBrand(event.target.value);
+    };
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="brand-select-label">Brand</InputLabel>
+            <Select
+            required
+            labelId="brand-select-label"
+            variant='filled'
+            id="brand-select"
+            value={selectedBrand}
+            label="Brand"
+            onChange={(e) => handleChange(e)}
+            >
+                {brands.map(brand => (
+                    <MenuItem key={brand.id} value={brand}>{brand.name}</MenuItem>
+                ))}
+            </Select>
+      </FormControl>
+    );
+};
+
+export default BrandSelector;
