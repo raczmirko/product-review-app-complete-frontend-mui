8d0a2cddc0e38b26456b5f81680090ce855f24cc
raczmirko <m1rk0d3v@gmail.com>
2024-05-19 14:46:43 +0200

    feat: added ArticleService

--- src/services/ArticleService.js
+++ src/services/ArticleService.js
@@ -20,6 +20,29 @@ class ArticleService {
             console.error('Error fetching article:', error);
             return [];
         }
+    };
+
+    static async fetchArticles() {
+        const token = localStorage.getItem('token');
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        };
+        try {
+            const response = await fetch(`http://localhost:8080/article/all`, {headers});
+            if (!response.ok) {
+                throw new Error('Failed to fetch articles.');
+            }
+            if (response.status === 204) {
+                return [];
+            } else {
+                const data = await response.json();
+                return data;
+            }
+        } catch (error) {
+            console.error('Error fetching articles:', error);
+            return [];
+        }
     }
 }
 
