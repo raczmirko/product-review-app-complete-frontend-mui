6b1ae629be6c64a140ea313ca146b5e277a8f98b
raczmirko <m1rk0d3v@gmail.com>
2024-05-19 16:17:35 +0200

    feat: added PackagingService

---
+++ src/services/PackagingService.js
@@ -0,0 +1,26 @@
+class PackagingService {
+    static async fetchPackagings() {
+        const token = localStorage.getItem('token');
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        };
+        try {
+            const response = await fetch(`http://localhost:8080/packaging/all`, {headers});
+            if (!response.ok) {
+                throw new Error('Failed to fetch packagings.');
+            }
+            if (response.status === 204) {
+                return [];
+            } else {
+                const data = await response.json();
+                return data;
+            }
+        } catch (error) {
+            console.error('Error fetching packagings:', error);
+            return [];
+        }
+    };
+}
+
+export default PackagingService;
