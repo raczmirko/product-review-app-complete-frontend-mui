25a9b7fd91f5f0f5ed06ccb161e4a4e9ed0f10f5
raczmirko <m1rk0d3v@gmail.com>
2024-05-19 16:14:29 +0200

    feat: DataDisplayTable has sticky header and custom height

--- src/components/tables/DataDisplayTable.jsx
+++ src/components/tables/DataDisplayTable.jsx
@@ -7,35 +7,38 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
-function DataDisplayTable({ data }) {
+function DataDisplayTable({ data, maxHeight }) {
     // Extract headers from the keys of the first object
     const headers = data.length > 0 ? Object.keys(data[0]) : [];
 
     return (
         <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="dynamic table">
-                <TableHead>
-                    <TableRow>
-                        {headers.map((header) => (
-                            <TableCell key={header} align="right">{header.toUpperCase()}</TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {data.map((row, rowIndex) => (
-                        <TableRow key={rowIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            <Box sx={{ maxHeight: maxHeight, overflow: 'auto' }}>
+                <Table stickyHeader aria-label="dynamic table" size="small" sx={{ minWidth: 650, maxHeight: 300 }}>
+                    <TableHead>
+                        <TableRow>
                             {headers.map((header) => (
-                                <TableCell key={header} align="right">
-                                    {typeof row[header] === 'object' && row[header] !== null
-                                        ? row[header].name
-                                        : row[header]}
-                                </TableCell>
+                                <TableCell key={header} align="right">{header.toUpperCase()}</TableCell>
                             ))}
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    </TableHead>
+                    <TableBody>
+                    {data.map((row, rowIndex) => (
+                            <TableRow key={rowIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                {headers.map((header) => (
+                                    <TableCell key={header} align="right">
+                                        {typeof row[header] === 'object' && row[header] !== null
+                                            ? row[header].name
+                                            : row[header]}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Box>
         </TableContainer>
     );
 }
