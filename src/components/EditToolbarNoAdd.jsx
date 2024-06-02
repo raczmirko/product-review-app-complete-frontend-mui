1763fbb34b731db31f9b182da5418f1902cd10cf
raczmirko <m1rk0d3v@gmail.com>
2024-06-02 15:18:15 +0200

    feat: added EditToolbarNoAdd for products because products can only be created from the Article table

---
+++ src/components/EditToolbarNoAdd.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { GridToolbarContainer, GridToolbar } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+
+function EditToolbar(props) {
+    const { setmodalactive, deleterecords, rowselectionmodel, showQuickFilter } = props;
+    return (
+        <GridToolbarContainer>
+            <GridToolbar showQuickFilter={showQuickFilter} />
+            <Box sx={{ marginLeft: 'auto' }}>
+                <Button color="primary" 
+                        startIcon={<DeleteIcon />} 
+                        onClick={() => deleterecords(rowselectionmodel)}
+                >
+                    Delete selected
+                </Button>
+            </Box>
+        </GridToolbarContainer>
+    );
+}
+
+export default EditToolbar;
