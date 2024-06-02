48f652ce35ab6e39dda020ba965603bc369bd05d
raczmirko <m1rk0d3v@gmail.com>
2024-06-02 11:18:43 +0200

    feat: added image view window to gallery with zoom functionality

--- src/components/modals/GalleryModal.jsx
+++ src/components/modals/GalleryModal.jsx
@@ -10,10 +10,14 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { apiRequest } from '../../services/CrudService';
 import ProductImageService from '../../services/ProductImageService';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function GalleryModal({ product, closeFunction, isOpen, setIsOpen }) {
     
     const [images, setImages] = useState([]);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [zoomLevel, setZoomLevel] = useState(1);
+    const [zoomPosition, setZoomPosition] = useState({ x: 0, y: 0 });
 
     const VisuallyHiddenInput = styled('input')({
         clip: 'rect(0 0 0 0)',
@@ -43,6 +47,21 @@ export default function GalleryModal({ product, closeFunction, isOpen, setIsOpen
         if(result.success) setImages(result.data.productImages);
     };
 
+    const deleteProductImage = async (imageIndex) => {
+        let imageId = images[imageIndex].id;
+        const endpoint = `http://localhost:8080/product-image/${imageId}/delete`;
+        const requestBody = undefined;
+        const result = await apiRequest(endpoint, 'POST', requestBody);
+        // Manually remove the image so that re-fetching all images is not neccessary
+        if (result.success) {
+            setImages((prevImages) => {
+                const updatedImages = [...prevImages];
+                updatedImages.splice(imageIndex, 1); // Remove the image at the specified index
+                return updatedImages;
+            });
+        }
+    };
+
     const uploadProductImages = async (productId, images) => {
         const response = await ProductImageService.uploadProductImages(productId, images);
         if (response.success) fetchProduct(product.id);
@@ -60,12 +79,16 @@ export default function GalleryModal({ product, closeFunction, isOpen, setIsOpen
         }
     };
 
-    const handleRemoveImage = (index) => {
-        setImages((prevImages) => {
-            const updatedImages = [...prevImages];
-            updatedImages.splice(index, 1); // Remove the image at the specified index
-            return updatedImages;
-        });
+    const handleMouseMove = (e) => {
+        const { left, top, width, height } = e.target.getBoundingClientRect();
+        const x = ((e.clientX - left) / width) * 100;
+        const y = ((e.clientY - top) / height) * 100;
+        setZoomPosition({ x, y });
+    };
+
+    const handleWheel = (e) => {
+        const delta = e.deltaY > 0 ? -0.1 : 0.1;
+        setZoomLevel((prevZoomLevel) => Math.min(Math.max(prevZoomLevel + delta, 1), 3));
     };
 
     const renderImageUploadButton = () => {
@@ -102,9 +125,8 @@ export default function GalleryModal({ product, closeFunction, isOpen, setIsOpen
                     width: '80%',
                     bgcolor: 'background.paper',
                     boxShadow: 24,
-                    p: 4,
+                    p: 6,
                     outline: '1px solid #81be83',
-                    textAlign: 'center',
                     display: 'flex',
                     flexDirection: 'column',
                     overflow: 'hidden'
@@ -113,34 +135,133 @@ export default function GalleryModal({ product, closeFunction, isOpen, setIsOpen
                 <Typography variant="h5" component="div" gutterBottom>
                     Product Images
                 </Typography>
-                <Box sx={{ overflowY: 'auto', marginBottom: 3 }}>
-                    <ImageList cols={4} >
-                        {images.map((item, index) => (
-                            <ImageListItem key={item.id} sx={{ m: 1, maxWidth: '300px', maxHeight: 'auto' }}>
-                            <img
-                                src={`data:image/png;base64,${item.image}`}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                            <Button
-                                variant="contained"
-                                color="error"
-                                sx={{
-                                    position: 'absolute',
-                                    top: 0,
-                                    right: 0,
-                                    zIndex: 1,
-                                    bg: 'red',
-                                }}
-                                onClick={() => handleRemoveImage(index)}
-                            >
-                                X
-                            </Button>
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
+                <Box sx={{ display: 'flex', flexGrow: 1, overflowY: 'auto' }}>
+                    <Box sx={{ width: '60%', overflowY: 'auto', mr: 2 }}>
+                        <ImageList cols={4} gap={12}>
+                            {images.map((item, index) => (
+                                <ImageListItem
+                                    key={item.id}
+                                    sx={{
+                                        position: 'relative',
+                                        transition: 'transform 0.3s ease-in-out',
+                                        '&:hover': {
+                                            transform: 'scale(1.1)',
+                                            zIndex: 999,
+                                            '& .magnifyIcon': {
+                                                display: 'block',
+                                            },
+                                        },
+                                    }}
+                                >
+                                    <img
+                                        src={`data:image/png;base64,${item.image}`}
+                                        alt={item.title}
+                                        loading="lazy"
+                                        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                    />
+                                    <Button
+                                        variant="contained"
+                                        color="error"
+                                        sx={{
+                                            position: 'absolute',
+                                            top: 0,
+                                            right: 0,
+                                            zIndex: 1,
+                                            bg: 'red',
+                                        }}
+                                        onClick={() => deleteProductImage(index)}
+                                    >
+                                        X
+                                    </Button>
+                                    <Button
+                                        className="magnifyIcon"
+                                        onClick={() => setSelectedImage(item.image)}
+                                        sx={{
+                                            display: 'none',
+                                            position: 'absolute',
+                                            top: '50%',
+                                            left: '50%',
+                                            transform: 'translate(-50%, -50%)',
+                                            zIndex: 2,
+                                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                                            color: 'white',
+                                            '&:hover': {
+                                                backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                                            },
+                                        }}
+                                    >
+                                        <SearchIcon fontSize='large'/>
+                                    </Button>
+                                </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </Box>
+                    <Box
+                        sx={{
+                            width: '40%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            border: '1px solid #ccc',
+                            padding: 2,
+                            position: 'relative',
+                            overflow: 'hidden',
+                        }}
+                        onMouseMove={handleMouseMove}
+                        onWheel={handleWheel}
+                    >
+                        {selectedImage ? (
+                            <Box sx={{ 
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                position: 'relative', 
+                                width: '100%', 
+                                height: '100%' 
+                            }}>
+                                <Typography 
+                                    variant="h4" 
+                                    sx={{ 
+                                        position: 'absolute', 
+                                        top: 0, 
+                                        width: '100%', 
+                                        textAlign: 'center', 
+                                        backgroundColor: 'rgba(255, 255, 255, 0.8)' 
+                                    }}
+                                >
+                                    Image preview
+                                </Typography>
+                                <img
+                                    src={`data:image/png;base64,${selectedImage}`}
+                                    alt="Selected"
+                                    style={{
+                                        width: '100%',
+                                        maxHeight: '100%',
+                                        transform: `scale(${zoomLevel})`,
+                                        transformOrigin: `${zoomPosition.x}% ${zoomPosition.y}%`,
+                                        transition: 'transform 0.1s',
+                                    }}
+                                />
+                                <Typography 
+                                    variant="subtitle1" 
+                                    sx={{ 
+                                        position: 'absolute', 
+                                        bottom: 0, 
+                                        width: '100%', 
+                                        textAlign: 'center', 
+                                        backgroundColor: 'rgba(255, 255, 255, 0.8)' 
+                                    }}
+                                >
+                                    Zoom by scrolling with the mouse wheel.
+                                </Typography>
+                            </Box>
+                        ) : (
+                            <Typography variant="h6">Hover over an image and click the magnifying glass to view</Typography>
+                        )}
+                    </Box>
                 </Box>
-                <Box sx={{ textAlign: 'right', mt: 1 }}>
+                <Box sx={{ textAlign: 'right', mt: 1, display: 'flex', justifyContent: 'flex-end' }}>
                     {renderImageUploadButton()}
                     <Button variant="contained" color="error" onClick={handleClose}>Close</Button>
                 </Box>
