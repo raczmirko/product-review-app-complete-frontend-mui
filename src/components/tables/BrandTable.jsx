05b77513f60c416129d6ca894124898f915a40f4
raczmirko <m1rk0d3v@gmail.com>
2024-06-09 11:22:23 +0200

    refactor: fixed indentation

--- src/components/tables/BrandTable.jsx
+++ src/components/tables/BrandTable.jsx
@@ -358,7 +358,7 @@ export default function BrandTable() {
             ))}
           </Select>
         );
-      }
+    }
       
     const renderSelectEditInputCell = (params) => {
         return <SelectEditInputCell {...params} />;
