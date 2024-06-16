8ba9739f04538c872f19ae4af5d6ab1abd772268
raczmirko <m1rk0d3v@gmail.com>
2024-06-16 09:58:54 +0200

    feat: NotificationService now only shows custom messages if the backend doesn't send any specific message

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -1,16 +1,16 @@
 class NotificationService {
     static getCustomNotification = (code, message) => {
-        let text = code + ": An error occurred, please try again later!";
-        if(code === 400) {
+        let text = code + ": " + message;
+        if(code === 400 && message === undefined) {
             text = code + ": BAD REQUEST. Something went wrong, please try again!";
         }
-        if(code === 401) {
+        if(code === 401 && message === undefined) {
             text = code + ": Authentication failed. Log in again!";
         }
-        if(code === 403) {
+        if(code === 403 && message === undefined) {
             text = code + ": You cannot access this page. Your session might have expired or you might need admin privileges to view.";
         }
-        if(code === 404) {
+        if(code === 404 && message === undefined) {
             text = code + ": NOT FOUND.";
         }
         if(code === 409) {
