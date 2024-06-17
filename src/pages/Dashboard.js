dcc355017b66044be88b20f39ba60d79ce5ef02d
raczmirko <m1rk0d3v@gmail.com>
2024-06-17 20:33:36 +0200

    feat: created dashboard page base

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,18 @@
-const Home = () => {
+import PageHeader from "../components/PageHeader";
+import Container from '@mui/material/Container';
+import ParticleBackground from '../components/ParticleBackground';
+
+const Dashboard = () => {
     return (
-            <div>
-                <p>Hello</p>
-            </div>
-        );
+        <Container component="main" sx={{ marginTop:'20px' , padding:'10px', justifyContent: 'center'}}>
+            <Container sx={{position: 'absolute', zIndex:-1}}>
+                <ParticleBackground />
+            </Container>
+            <Container >
+                <PageHeader text="Dashboard" color="#81BE83" textColor="white"/>
+             </Container>
+        </Container>
+    );
 }
 
-export default Home;
+export default Dashboard;
