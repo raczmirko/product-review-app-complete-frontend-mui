0364a03ad2bd29bc1d38d7fe07945b153ef0f9bc
raczmirko <m1rk0d3v@gmail.com>
2024-06-22 16:28:43 +0200

    feat: added "No data available" placeholder when failed to fetch data for DataDisplayTable

--- src/components/tables/DataDisplayTable.jsx
+++ src/components/tables/DataDisplayTable.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 function DataDisplayTable({ data, maxHeight }) {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5); // Default rows per page
 
     // Extract headers from the keys of the first object
-    const headers = data.length > 0 ? Object.keys(data[0]) : [];
+    const headers = data?.length > 0 ? Object.keys(data[0]) : [];
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -27,51 +28,59 @@ function DataDisplayTable({ data, maxHeight }) {
     };
 
     // Calculate the rows to be displayed based on the current page and rows per page
-    const displayedRows = data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+    const displayedRows = data?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
 
     // Calculate the number of empty rows to avoid layout jumps
-    const emptyRows = Math.max(0, (1 + page) * rowsPerPage - data.length);
+    const emptyRows = Math.max(0, (1 + page) * rowsPerPage - data?.length);
 
     return (
         <TableContainer component={Paper}>
-            <Box sx={{ maxHeight: maxHeight, overflow: 'auto' }}>
-                <Table stickyHeader aria-label="dynamic table" size="small" sx={{ minWidth: 650, maxHeight: 300 }}>
-                    <TableHead>
-                        <TableRow>
-                            {headers.map((header) => (
-                                <TableCell key={header} align="left">{header.toUpperCase()}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {displayedRows.map((row, rowIndex) => (
-                            <TableRow key={rowIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            {data && data.length > 0 ? (
+                <Box sx={{ maxHeight: maxHeight, overflow: 'auto' }}>
+                    <Table stickyHeader aria-label="dynamic table" size="small" sx={{ minWidth: 650, maxHeight: 300 }}>
+                        <TableHead>
+                            <TableRow>
                                 {headers.map((header) => (
-                                    <TableCell key={header} align="left">
-                                        {typeof row[header] === 'object' && row[header] !== null
-                                            ? row[header].name
-                                            : row[header]}
-                                    </TableCell>
+                                    <TableCell key={header} align="left">{header.toUpperCase()}</TableCell>
                                 ))}
                             </TableRow>
-                        ))}
-                        {emptyRows > 0 && (
-                            <TableRow style={{ height: (33 * emptyRows) }}>
-                                <TableCell colSpan={headers.length} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </Box>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={data.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
+                        </TableHead>
+                        <TableBody>
+                            {displayedRows.map((row, rowIndex) => (
+                                <TableRow key={rowIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                    {headers.map((header) => (
+                                        <TableCell key={header} align="left">
+                                            {typeof row[header] === 'object' && row[header] !== null
+                                                ? row[header].name
+                                                : row[header]}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: (33 * emptyRows) }}>
+                                    <TableCell colSpan={headers.length} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </Box>
+            ) : (
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: maxHeight }}>
+                    <Typography variant="h6" color="textSecondary">No data available</Typography>
+                </Box>
+            )}
+            {data && data.length > 0 && (
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={data.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            )}
         </TableContainer>
     );
 }
